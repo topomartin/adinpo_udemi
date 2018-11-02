afaf8459b7c019f9423271a392059a8ef1f74265
topomartin <martin.am2@gmail.com>
2018-11-02 09:00:14 +0100

    Modigicaciones en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,14 +14,6 @@ import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-    { path: '',
-     component: PagesComponent,
-     children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progres', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
-    ] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
